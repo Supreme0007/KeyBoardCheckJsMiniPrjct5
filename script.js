04531ac2262a2b621e9889464849ff92f48ced5c
Suraj Singh <rajputss8223@gmail.com>
2023-11-24 11:53:56 +0530

    jsMiniPrjct

---
+++ script.js
@@ -0,0 +1,20 @@
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+    <div class="table">
+    <table>
+  <tr>
+    <th>KEY</th>
+    <th>KEYCODE</th>
+    <th>CODE</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' ' ? 'Space' : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+    </div>
+    `
+});
